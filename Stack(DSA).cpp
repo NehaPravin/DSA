65f7afa9378eafe8cbaab871a22b51282640b3d5
Neha Pravin <104181302+NehaPravin@users.noreply.github.com>
2022-09-23 06:13:37 +0545

    Add files via upload

---
+++ Stack(DSA).cpp
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+	int stack[5],top=1;
+	void push();
+	void pop();
+	void show();
+	int main()
+	{
+	 int ch;
+	 printf("1.push \n");
+	 printf("2.pop \n")	;
+	 printf("3.show\n");
+	 printf("4.exit \n");
+	 while(1)
+	 {
+	 	printf("\n enter your number");
+	 	scanf("%d",&ch);
+	 	switch(ch)
+	 	{
+	 		case 1:
+	 			push();
+	 			break;
+	 		case 2:
+	 				pop();
+	 				break;
+	 		case 3:
+	 					show();
+	 					break;
+	 		case 4:
+	 					exit(0);
+	 					break;
+	 					
+	 		default:
+	 					printf("invalid option");
+	 							
+	 				
+		 }
+	 }
+	 
+	 return 0;
+	 
+	}
+	void push()
+		{
+		int item ;
+		if(top==4-1)
+		{
+			printf("stack is full");
+			
+		}
+		else
+		{
+			printf("push element in stack ");
+			scanf("%d",&item);
+			top=top+1;
+			
+			stack[top]=item;
+			
+		}
+	}
+void pop()
+{
+	if(top==-1)
+	{
+		printf("stack is empty");
+		
+	}
+	
+	else
+	{
+		printf("popped %d ",stack[top]);
+		top=top-1;
+	}
+}
+void show()
+{
+	int i;
+	if(top>=0)
+	{
+		printf("stack element \n");
+		for(i=top;i>0;i--)
+		{
+			printf("%d",stack[i]);
+			
+		}
+	}
+		else
+		{
+			printf("stack is empty");
+			
+		}
+	}
+
